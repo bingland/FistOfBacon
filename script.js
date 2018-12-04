e2c7622a40b18e51f5dc4e139a9c841d72697b48
daxholmes <44475327+daxholmes@users.noreply.github.com>
2018-12-04 11:09:01 -0700

    Removed Timeline

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ function drawChart() {
     ],
 
     rows: [
-      {c: [{v: '1300 BC: Chinese Invent Bacon.'},     {v: 'Date(1300, 0, 1)'}, {v: 'Date(1301, 0, 1)'}]},
+      {c: [{v: '1300 BC: Chinese Invent Bacon.'},     {v: 'Date(0001, 0, 1)'}, {v: 'Date(0002, 0, 1)'}]},
       {c: [{v: 'Romans eat a type of bacon called petaso.'}, {v: 'Date(1500, 0, 1)'}, {v: 'Date(1501, 0, 1)'}]},
       {c: [{v: 'First Bacon factory opend in England.'}, {v: 'Date(1770, 0, 1)'}, {v: 'Date(1771, 0, 1)'}]},
       {c: [{v: 'World War II: Bacon grease donated by households for munitions.'}, {v: 'Date(1939, 8, 3)'}, {v: 'Date(1945, 8, 2)'}]},
-      {c: [{v: 'World War II: Bacon grease donated by households for munitions.'}, {v: 'Date(1992, 0, 1)'}, {v: 'Date(1993, 0, 1)'}]}
+      {c: [{v: 'World War II: Bacon grease donated by households for munitions.'}, {v: 'Date(1992, 0, 1)'}, {v: 'Date(1993, 0, 1)'}]},
+      {c: [{v: 'United Church of Bacon founded'}, {v: 'Date(2010, 0, 1)'}, {v: 'Date(2018, 0, 1)'}]}
     ]
   });
 
