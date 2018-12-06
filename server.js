69082ccc4bd3fc11a9bef1d79a207d46e5c3aace
Brayden England <35476877+snotbubbler@users.noreply.github.com>
2018-12-06 11:10:56 -0700

    Optimized server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require("express");
 
 var app = express();
 
-app.use("/",express.static(__dirname + "public"));
+app.use("/",express.static("public"));
 
-app.listen(3000);
+app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
+var server = app.listen(process.env.PORT || 8080, function(){
+    var port = server.address().port;
+    console.log("App now running on port ", port);
+});
